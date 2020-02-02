90cb1544a7b37242fb94acc66564dfa106e5cdf9
Alexander Pietsch <alexander.pietsch@googlemail.com>
2020-02-02 22:03:04 +0100

    refactored demo methods an introduced async methods calls for external rest services

--- src/main/java/be/pengo/tomeeapi/AnyUserService.java
+++ src/main/java/be/pengo/tomeeapi/AnyUserService.java
@@ -18,8 +18,7 @@ import javax.ws.rs.core.Response;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.*;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.Future;
+import java.util.concurrent.*;
 import java.util.stream.Stream;
 
 import static java.util.concurrent.CompletableFuture.supplyAsync;
@@ -32,117 +31,124 @@ public class AnyUserService {
     private static final String HTTP_JSONPLACEHOLDER_TYPICODE_COM = "http://jsonplaceholder.typicode.com";
 
     @GET
-    @Path("long_taking_service")
+    @Path("anyuser/{userid}")
     @Produces({APPLICATION_JSON})
-    public Response getLongTaskResult() {
-        System.out.println("Handle rest request in Thread: " + Thread.currentThread().getName());
-        getResultOverNetwork();
+    public Response getAnyUser(@PathParam("userid") Long userId) {
+        System.out.println(String.format("Service method getAnyUser() lives in Thread %s", Thread.currentThread().getName()));
         Instant start = Instant.now();
-        doCompletableFutureStuff();
-        Instant stop = Instant.now();
-        long duration = Duration.between(start, stop).toMillis();
-        return Response.ok().entity("end long processing in " + duration / 1000 + " seconds.").build();
-    }
-
-    @GET
-    @Path("content/{AUTHOR_ID}")
-    @Produces({APPLICATION_JSON})
-    public Response getAnyUser(@PathParam("AUTHOR_ID") Long authorId){
-        AnyUser anyUser = queryAnyUser(authorId);
+        AnyUser anyUser = queryAnyUser(userId);
         if (anyUser == null) {
             return Response.status(Response.Status.NOT_FOUND).build();
         }
 
-        List<AnyUserPost> anyUserPosts = queryAnyUsersPosts(authorId);
+        List<AnyUserPost> anyUserPosts = queryAnyUsersPosts(userId);
         if (anyUserPosts != null) {
             anyUser.setPostList(anyUserPosts);
         }
 
-        if (anyUser == null) {
-            return Response.status(Response.Status.NOT_FOUND).build();
-        }
+        Instant stop = Instant.now();
+        long runtime = Duration.between(start, stop).toMillis();
+        System.out.println("Service Method getAnyUser() took " + runtime + " milliseconds to complete");
 
         return Response.ok().entity(anyUser).build();
     }
 
-    private AnyUser queryAnyUser(Long authorId){
-        WebTarget target = ClientBuilder.newClient().target(HTTP_JSONPLACEHOLDER_TYPICODE_COM);
-        Response response = target.path("users").path(authorId.toString()).request().accept(MediaType.APPLICATION_JSON).get();
+    @GET
+    @Path("asyncanyuser/{userid}")
+    @Produces({APPLICATION_JSON})
+    public Response getAnyUserAsync(@PathParam("userid") Long userId) {
+        System.out.println(String.format("Service method getAnyUserAsync() lives in Thread %s", Thread.currentThread().getName()));
+        Instant start = Instant.now();
+        AnyUser responseAnyUser = null;
+        try {
+            CompletableFuture<AnyUser> anyUserFuture = supplyAsync(() -> queryAnyUser(userId)).
+                    exceptionally(e -> {
+                        System.out.println("An exception happened in queryAnyUser: " + e.getMessage());
+                        e.printStackTrace();
+                        return null;
+                    }).
+                    thenCombine(supplyAsync(() -> queryAnyUsersPosts(userId)).
+                                    exceptionally(e -> {
+                                        System.out.println("An exception happened in queryAnyUsersPosts: " + e.getMessage());
+                                        e.printStackTrace();
+                                        return null;
+                                    }),
+                            (anyUser, anyUserPosts) -> {
+                                anyUser.setPostList(anyUserPosts);
+                                return anyUser;
+                            }).exceptionally(e -> {
+                System.out.println("An exception happened in combine: " + e.getMessage());
+                e.printStackTrace();
+                return null;
+            });
+            responseAnyUser = anyUserFuture.get(10, TimeUnit.SECONDS);
+        } catch (InterruptedException | ExecutionException | TimeoutException e) {
+            System.out.println("Something went wrong for anyUserFuture CompletableFuture: " + e.getMessage());
+            e.printStackTrace();
+        }
 
-        if(response.getStatusInfo().getFamily().equals(Response.Status.Family.SUCCESSFUL)) {
+        Instant stop = Instant.now();
+        long runtime = Duration.between(start, stop).toMillis();
+        System.out.println("Service Method getAnyUserAsync() took " + runtime / 1000 + " seconds to complete");
+
+        if (responseAnyUser == null) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+        return Response.ok().entity(responseAnyUser).build();
+    }
+
+    private AnyUser queryAnyUser(Long authorId) {
+        System.out.println(String.format("Method queryAnyUser() lives in Thread %s", Thread.currentThread().getName()));
+        Instant start = Instant.now();
+        threadSleepForSeconds(3);
+        WebTarget target = ClientBuilder.newClient().target(HTTP_JSONPLACEHOLDER_TYPICODE_COM);
+        Response response = target.path("users").
+                path(authorId.toString()).
+                request().
+                accept(MediaType.APPLICATION_JSON).get();
+
+        Instant stop = Instant.now();
+        long runtime = Duration.between(start, stop).toMillis();
+        System.out.println("Method queryAnyUser() took " + runtime / 1000 + " seconds to complete");
+
+        if (response.getStatusInfo().getFamily().equals(Response.Status.Family.SUCCESSFUL)) {
             String asString = response.readEntity(String.class);
             Mapper mapper = new MapperBuilder().build();
-            AnyUser anyUser = mapper.readObject(asString, AnyUser.class);
-            return anyUser;
+            return mapper.readObject(asString, AnyUser.class);
         }
+
         return null;
     }
 
-    private List<AnyUserPost> queryAnyUsersPosts(Long authorId){
+    private List<AnyUserPost> queryAnyUsersPosts(Long authorId) {
+        System.out.println(String.format("Method queryAnyUsersPosts() lives in Thread %s", Thread.currentThread().getName()));
+        Instant start = Instant.now();
+        threadSleepForSeconds(3);
         WebTarget target = ClientBuilder.newClient().target(HTTP_JSONPLACEHOLDER_TYPICODE_COM);
         Response response = target.path("posts").
                 queryParam("userId", authorId).
                 request().accept(MediaType.APPLICATION_JSON).get();
-        if(response.getStatusInfo().getFamily().equals(Response.Status.Family.SUCCESSFUL)) {
+
+        Instant stop = Instant.now();
+        long runtime = Duration.between(start, stop).toMillis();
+        System.out.println("Method queryAnyUsersPosts() took " + runtime / 1000 + " seconds to complete");
+
+        if (response.getStatusInfo().getFamily().equals(Response.Status.Family.SUCCESSFUL)) {
             String asString = response.readEntity(String.class);
             Jsonb jsonb = new JohnzonBuilder().build();
-            List<AnyUserPost> personList = jsonb.fromJson(asString, new ArrayList<AnyUserPost>(){}.getClass().getGenericSuperclass());
-            return personList;
+            return jsonb.fromJson(asString,
+                    new ArrayList<AnyUserPost>() {
+                    }.getClass().getGenericSuperclass());
         }
         return null;
     }
 
-    private void doCompletableFutureStuff() {
-        CompletableFuture<Void> future = supplyAsync(() -> getFruits()).
-                thenCombine(supplyAsync(() -> getVeggies()),
-                        (fruits, veggies) -> {
-                            System.out.println("Method doCompletableFutureStuff() combine Thread is: " + Thread.currentThread().getName());
-                            Stream<String> concat = Stream.concat(Arrays.stream(fruits), Arrays.stream(veggies));
-                            return concat;
-                        }
-                ).thenAcceptAsync(items -> {
-                    System.out.println("Method thenAcceptAsync() Thread is: " + Thread.currentThread().getName());
-                    items.forEach(System.out::println);
-                }
-        );
-        future.join();
-    }
-
-    public Future<String> getResultOverNetwork() {
-        CompletableFuture<String> future = new CompletableFuture<>();
-        new Thread(() -> {
-            delay(2000, "getResultOverNetwork"); //simulate network call
-            future.complete("{\"result\":\"success\"}");
-        }).start();
-        return future;
-    }
-
-    private void delay(int sleepInMillis, String name) {
+    private void threadSleepForSeconds(int seconds) {
+        System.out.println(String.format("Intentional sleep for %s seconds in Thread: %s ", seconds, Thread.currentThread().getName()));
         try {
-            System.out.println(String.format("Latency simulation for method %s (Thread %s goes to sleep for %s seconds..)", name, Thread.currentThread().getName(), sleepInMillis / 1000));
-            Thread.sleep(sleepInMillis);
-            System.out.println(String.format("Latency simulation for method %s ends (Thread  %s wakes up (end of work unit) ..) ", name , Thread.currentThread().getName()));
+            Thread.sleep(seconds * 1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-    }
-
-    public String[] getFruits() {
-        Instant start = Instant.now();
-        delay(30000, "Method getFruits()"); //simulate network latency
-        Instant stop = Instant.now();
-        long runtime = Duration.between(start, stop).toMillis();
-        System.out.println("Method getFruits() took " + runtime / 1000 + " seconds to complete");
-        return new String[]{"apple", "apricot", "banana"};
-    }
-
-    public String[] getVeggies() {
-        Instant start = Instant.now();
-        delay(10000, "Method getVeggies()"); //simulate network latency
-        Instant stop = Instant.now();
-        long runtime = Duration.between(start, stop).toMillis();
-        System.out.println("Method getVeggies() took " + runtime / 1000 + " seconds to complete");
-
-        return new String[]{"broccoli", "brussels sprout"};
     }
 }
--- src/test/java/be/pengo/tomeeapi/AnyUserServiceTest.java
+++ src/test/java/be/pengo/tomeeapi/AnyUserServiceTest.java
@@ -16,9 +16,9 @@
  */
 package be.pengo.tomeeapi;
 
-import org.hamcrest.CoreMatchers;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.junit.InSequence;
 import org.jboss.arquillian.test.api.ArquillianResource;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
@@ -27,23 +27,85 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.time.Duration;
-import java.time.Instant;
 
 @RunWith(Arquillian.class)
 public class AnyUserServiceTest extends Assert {
 
-    @PersistenceContext(name = "myPU")
-    protected EntityManager entityManager;
-    
+    @ArquillianResource
+    private URL webappUrl;
+
+    @Test
+    @InSequence(1)
+    public void testAnyUserService() throws Exception {
+        AnyUser anyUser = getResponse("api/async/anyuser/1", AnyUser.class);
+        assertNotNull(anyUser);
+        assertEquals(10, anyUser.getPostList().size());
+        assertEquals(Long.valueOf(1), anyUser.getId());
+        assertEquals("Leanne Graham", anyUser.getName());
+    }
+
+
+    @Test
+    @InSequence(2)
+    public void testAsyncAnyUserService() throws Exception {
+        AnyUser anyUser = getResponse("api/async/asyncanyuser/1", AnyUser.class);
+        assertNotNull(anyUser);
+        assertEquals(10, anyUser.getPostList().size());
+        assertEquals(Long.valueOf(1), anyUser.getId());
+        assertEquals("Leanne Graham", anyUser.getName());
+    }
+
+    @Test
+    @InSequence(3)
+    public void testAnyUserJsonResponse() throws URISyntaxException {
+        AnyUser anyUser = getResponse("api/async/asyncanyuser/1", AnyUser.class);
+        assertNotNull(anyUser);
+        assertEquals(10, anyUser.getPostList().size());
+        assertEquals(Long.valueOf(1), anyUser.getId());
+        assertEquals("Leanne Graham", anyUser.getName());
+    }
+
+    @Test
+    @InSequence(4)
+    public void testUnknownAnyUserJsonResponse() throws URISyntaxException {
+        WebTarget webTarget = ClientBuilder.newClient().target(webappUrl.toURI());
+        Response response = webTarget.path("api/async/asyncanyuser/300").
+                request().
+                accept(MediaType.APPLICATION_JSON).
+                get();
+        assertEquals(Response.Status.NOT_FOUND, Response.Status.fromStatusCode(response.getStatus()));
+    }
+
+    @Test
+    @InSequence(5)
+    public void testAnyUserAttributeOrderFromJSONResponse() throws URISyntaxException {
+        Response response = getResponse("api/async/asyncanyuser/1");
+        String readEntity = response.readEntity(String.class);
+        assertEquals("{\"id\":1,\"name\":\"Leanne Graham\",\"postList\":[{\"body\":\"quia et suscipit\\nsuscipit recusandae consequuntur expedita et cum\\nreprehenderit molestiae ut ut quas totam\\nnostrum rerum est autem sunt rem eveniet architecto\",\"id\":1,\"title\":\"sunt aut facere repellat provident occaecati excepturi optio reprehenderit\",\"userId\":1},{\"body\":\"est rerum tempore vitae\\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\\nqui aperiam non debitis possimus qui neque nisi nulla\",\"id\":2,\"title\":\"qui est esse\",\"userId\":1},{\"body\":\"et iusto sed quo iure\\nvoluptatem occaecati omnis eligendi aut ad\\nvoluptatem doloribus vel accusantium quis pariatur\\nmolestiae porro eius odio et labore et velit aut\",\"id\":3,\"title\":\"ea molestias quasi exercitationem repellat qui ipsa sit aut\",\"userId\":1},{\"body\":\"ullam et saepe reiciendis voluptatem adipisci\\nsit amet autem assumenda provident rerum culpa\\nquis hic commodi nesciunt rem tenetur doloremque ipsam iure\\nquis sunt voluptatem rerum illo velit\",\"id\":4,\"title\":\"eum et est occaecati\",\"userId\":1},{\"body\":\"repudiandae veniam quaerat sunt sed\\nalias aut fugiat sit autem sed est\\nvoluptatem omnis possimus esse voluptatibus quis\\nest aut tenetur dolor neque\",\"id\":5,\"title\":\"nesciunt quas odio\",\"userId\":1},{\"body\":\"ut aspernatur corporis harum nihil quis provident sequi\\nmollitia nobis aliquid molestiae\\nperspiciatis et ea nemo ab reprehenderit accusantium quas\\nvoluptate dolores velit et doloremque molestiae\",\"id\":6,\"title\":\"dolorem eum magni eos aperiam quia\",\"userId\":1},{\"body\":\"dolore placeat quibusdam ea quo vitae\\nmagni quis enim qui quis quo nemo aut saepe\\nquidem repellat excepturi ut quia\\nsunt ut sequi eos ea sed quas\",\"id\":7,\"title\":\"magnam facilis autem\",\"userId\":1},{\"body\":\"dignissimos aperiam dolorem qui eum\\nfacilis quibusdam animi sint suscipit qui sint possimus cum\\nquaerat magni maiores excepturi\\nipsam ut commodi dolor voluptatum modi aut vitae\",\"id\":8,\"title\":\"dolorem dolore est ipsam\",\"userId\":1},{\"body\":\"consectetur animi nesciunt iure dolore\\nenim quia ad\\nveniam autem ut quam aut nobis\\net est aut quod aut provident voluptas autem voluptas\",\"id\":9,\"title\":\"nesciunt iure omnis dolorem tempora et accusantium\",\"userId\":1},{\"body\":\"quo et expedita modi cum officia vel magni\\ndoloribus qui repudiandae\\nvero nisi sit\\nquos veniam quod sed accusamus veritatis error\",\"id\":10,\"title\":\"optio molestias id quia eum\",\"userId\":1}],\"username\":\"Bret\",\"address\":{\"zipcode\":\"92998-3874\",\"geo\":{\"lng\":\"81.1496\",\"lat\":\"-37.3159\"},\"suite\":\"Apt. 556\",\"city\":\"Gwenborough\",\"street\":\"Kulas Light\"},\"company\":{\"name\":\"Romaguera-Crona\",\"bs\":\"harness real-time e-markets\",\"catchPhrase\":\"Multi-layered client-server neural-net\"},\"email\":\"Sincere@april.biz\",\"phone\":\"1-770-736-8031 x56442\",\"website\":\"hildegard.org\"}", readEntity);
+    }
+
+    private <T> T getResponse(String path, Class<T> dtoClazz) throws URISyntaxException {
+        WebTarget webTarget = ClientBuilder.newClient().target(webappUrl.toURI());
+        return webTarget.path(path).
+                request().
+                accept(MediaType.APPLICATION_JSON).
+                get(dtoClazz);
+    }
+
+    private Response getResponse(String path) throws URISyntaxException {
+        WebTarget webTarget = ClientBuilder.newClient().target(webappUrl.toURI());
+        return webTarget.path(path).
+                request().
+                accept(MediaType.APPLICATION_JSON).
+                get();
+    }
+
     /**
      * ShrinkWrap is used to create a war file on the fly.
      *
@@ -72,61 +134,5 @@ public class AnyUserServiceTest extends Assert {
      * the name of the webapp
      *
      */
-    @ArquillianResource
-    private URL webappUrl;
-
-    @Test
-    public void testAsynService() throws Exception {
-        WebTarget webTarget = ClientBuilder.newClient().target(webappUrl.toURI());
-        Instant start = Instant.now();
-        Response response = webTarget.path("api/async/long_taking_service").request().get();
-        Instant stop = Instant.now();
-        long duration = Duration.between(start, stop).toMillis();
-        assertTrue(duration > 25000);
-        String readEntity = response.readEntity(String.class);
-        assertThat(readEntity, CoreMatchers.containsString("end long processing in"));
-    }
-
-    @Test
-    public void testAuthorContentJsonResponse() throws URISyntaxException {
-        AnyUser anyUser = getResponse("api/async/content/1", AnyUser.class);
-        assertNotNull(anyUser);
-        assertEquals(10, anyUser.getPostList().size());
-        assertEquals(Long.valueOf(1), anyUser.getId());
-        assertEquals("Leanne Graham", anyUser.getName());
-    }
-
-    @Test
-    public void testUnknownAuthorContentJsonResponse() throws URISyntaxException {
-        WebTarget webTarget = ClientBuilder.newClient().target(webappUrl.toURI());
-        Response response = webTarget.path("api/async/content/300").
-                request().
-                accept(MediaType.APPLICATION_JSON).
-                get();
-        assertEquals(Response.Status.NOT_FOUND, Response.Status.fromStatusCode(response.getStatus()));
-    }
-
-    @Test
-    public void testAuthorContentAttributeOrderFromJSONResponse() throws URISyntaxException {
-        Response response = getResponse("api/async/content/1");
-        String readEntity = response.readEntity(String.class);
-        assertEquals("{\"id\":1,\"name\":\"Leanne Graham\",\"postList\":[{\"body\":\"quia et suscipit\\nsuscipit recusandae consequuntur expedita et cum\\nreprehenderit molestiae ut ut quas totam\\nnostrum rerum est autem sunt rem eveniet architecto\",\"id\":1,\"title\":\"sunt aut facere repellat provident occaecati excepturi optio reprehenderit\",\"userId\":1},{\"body\":\"est rerum tempore vitae\\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\\nqui aperiam non debitis possimus qui neque nisi nulla\",\"id\":2,\"title\":\"qui est esse\",\"userId\":1},{\"body\":\"et iusto sed quo iure\\nvoluptatem occaecati omnis eligendi aut ad\\nvoluptatem doloribus vel accusantium quis pariatur\\nmolestiae porro eius odio et labore et velit aut\",\"id\":3,\"title\":\"ea molestias quasi exercitationem repellat qui ipsa sit aut\",\"userId\":1},{\"body\":\"ullam et saepe reiciendis voluptatem adipisci\\nsit amet autem assumenda provident rerum culpa\\nquis hic commodi nesciunt rem tenetur doloremque ipsam iure\\nquis sunt voluptatem rerum illo velit\",\"id\":4,\"title\":\"eum et est occaecati\",\"userId\":1},{\"body\":\"repudiandae veniam quaerat sunt sed\\nalias aut fugiat sit autem sed est\\nvoluptatem omnis possimus esse voluptatibus quis\\nest aut tenetur dolor neque\",\"id\":5,\"title\":\"nesciunt quas odio\",\"userId\":1},{\"body\":\"ut aspernatur corporis harum nihil quis provident sequi\\nmollitia nobis aliquid molestiae\\nperspiciatis et ea nemo ab reprehenderit accusantium quas\\nvoluptate dolores velit et doloremque molestiae\",\"id\":6,\"title\":\"dolorem eum magni eos aperiam quia\",\"userId\":1},{\"body\":\"dolore placeat quibusdam ea quo vitae\\nmagni quis enim qui quis quo nemo aut saepe\\nquidem repellat excepturi ut quia\\nsunt ut sequi eos ea sed quas\",\"id\":7,\"title\":\"magnam facilis autem\",\"userId\":1},{\"body\":\"dignissimos aperiam dolorem qui eum\\nfacilis quibusdam animi sint suscipit qui sint possimus cum\\nquaerat magni maiores excepturi\\nipsam ut commodi dolor voluptatum modi aut vitae\",\"id\":8,\"title\":\"dolorem dolore est ipsam\",\"userId\":1},{\"body\":\"consectetur animi nesciunt iure dolore\\nenim quia ad\\nveniam autem ut quam aut nobis\\net est aut quod aut provident voluptas autem voluptas\",\"id\":9,\"title\":\"nesciunt iure omnis dolorem tempora et accusantium\",\"userId\":1},{\"body\":\"quo et expedita modi cum officia vel magni\\ndoloribus qui repudiandae\\nvero nisi sit\\nquos veniam quod sed accusamus veritatis error\",\"id\":10,\"title\":\"optio molestias id quia eum\",\"userId\":1}],\"username\":\"Bret\",\"address\":{\"zipcode\":\"92998-3874\",\"geo\":{\"lng\":\"81.1496\",\"lat\":\"-37.3159\"},\"suite\":\"Apt. 556\",\"city\":\"Gwenborough\",\"street\":\"Kulas Light\"},\"company\":{\"name\":\"Romaguera-Crona\",\"bs\":\"harness real-time e-markets\",\"catchPhrase\":\"Multi-layered client-server neural-net\"},\"email\":\"Sincere@april.biz\",\"phone\":\"1-770-736-8031 x56442\",\"website\":\"hildegard.org\"}", readEntity);
-    }
-
-    private <T> T getResponse(String path, Class<T> dtoClazz) throws URISyntaxException {
-        WebTarget webTarget = ClientBuilder.newClient().target(webappUrl.toURI());
-        return webTarget.path(path).
-                request().
-                accept(MediaType.APPLICATION_JSON).
-                get(dtoClazz);
-    }
-
-    private Response getResponse(String path) throws URISyntaxException {
-        WebTarget webTarget = ClientBuilder.newClient().target(webappUrl.toURI());
-        return webTarget.path(path).
-                request().
-                accept(MediaType.APPLICATION_JSON).
-                get();
-    }
 
 }
